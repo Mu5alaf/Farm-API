76b1bf86967a690b6b772afe43610b3c42c40927
Mu5alaf <madmedo73@gmail.com>
2024-08-26 14:48:31 +0300

    fix view

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+#using AbstractUser creating your custom User model.
 class User(AbstractUser):
     phone_number = models.CharField(max_length=30)
     address = models.TextField(max_length=100)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics, permissions
-from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 from .serializers import UserSerializer, RegisterSerializer
 
